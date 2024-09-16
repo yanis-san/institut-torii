e2e323057a447c35b866f87d5937ef870fb4ee6c
yanis-san <barbarayanis14@gmail.com>
2024-09-16 15:18:34 +0100

    add form field for captcha

--- torii/contact/forms.py
+++ torii/contact/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 from torii.contact.models import Contact
+from django_recaptcha.fields import ReCaptchaField
 
 class ContactForm(forms.ModelForm):
+
+    captcha = ReCaptchaField()
     class Meta:
         model = Contact
         fields = ['name', 'email', 'phone_number', "why_contact_us"]
