2e1400f8f4199ed9fdecaa37d7aba7e001513126
yanis-san <barbarayanis14@gmail.com>
2024-10-08 12:29:34 +0100

    remove email functionnality

--- torii/contact/views.py
+++ torii/contact/views.py
@@ -13,58 +13,47 @@ def contact_form(request):
             contact.save()
             
             # Préparer le message HTML pour l'email de notification
-            html_message = render_to_string('contact/contact_email.html', {
-                'name': contact.name,
-                'email': contact.email,
-                'phone_number': contact.phone_number,
-                'why_contact_us': contact.why_contact_us,
-                'reason_for_contact': contact.reason_for_contact,
-            })
+            # html_message = render_to_string('contact/contact_email.html', {
+            #     'name': contact.name,
+            #     'email': contact.email,
+            #     'phone_number': contact.phone_number,
+            #     'why_contact_us': contact.why_contact_us,
+            #     'reason_for_contact': contact.reason_for_contact,
+            # })
             
             # Convertir le message HTML en texte brut
-            plain_message = strip_tags(html_message)
+            # plain_message = strip_tags(html_message)
             
             # Envoyer un email de notification
-            send_mail(
-                subject='Nouveau message de contact',
-                message=plain_message,
-                from_email='charger.com.igg@gmail.com',  
-                recipient_list=['smm.torii@gmail.com', 'charger.com.igg@gmail.com'],
-                #recipient_list=['smm.torii@gmail.com'],
-                fail_silently=False,
-                html_message=html_message,
-            )
+            # send_mail(
+            #     subject='Nouveau message de contact',
+            #     message=plain_message,
+            #     from_email='charger.igg.com@gmail.com',  
+            #     recipient_list=['smm.torii@gmail.com', 'charger.com.igg@gmail.com'],
+            #     fail_silently=False,
+            #     html_message=html_message,
+            # )
             
             # Préparer le message HTML pour l'email de confirmation
-            confirmation_html_message = render_to_string('contact/confirmation_email.html', {
-                'name': contact.name,
-            })
+            # confirmation_html_message = render_to_string('contact/confirmation_email.html', {
+            #     'name': contact.name,
+            # })
             
             # Convertir le message HTML en texte brut
-            confirmation_plain_message = strip_tags(confirmation_html_message)
+            # confirmation_plain_message = strip_tags(confirmation_html_message)
             
             # Envoyer un email de confirmation
-            send_mail(
-                subject='Nous avons bien reçu ta demande',
-                message=confirmation_plain_message,
-                from_email='charger.com.igg@gmail.com',
-                recipient_list=[contact.email],
-                fail_silently=False,
-                html_message=confirmation_html_message,
-            )
+            # send_mail(
+            #     subject='Nous avons bien reçu ta demande',
+            #     message=confirmation_plain_message,
+            #     from_email='charger.igg.com@gmail.com',
+            #     recipient_list=[contact.email],
+            #     fail_silently=False,
+            #     html_message=confirmation_html_message,
+            # )
             
             # Rediriger
             return redirect("home:homepage")
     else:
         form = ContactForm()
     return render(request, "contact/contact_form.html", context={"form": form})
-
-
-
-
-
-
-
-
-
-
